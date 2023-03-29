a7ca6877159d3a4a21a68cececb5d48e45c3eab0
Aces <nicolas76.breant@otmail.fr>
2023-03-29 16:10:29 +0200

    Functions of the calculator

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ let secondNumber = "";
 let operator = "";
 
 function operate(firstNumber, secondNumber, operator) {
+    firstNumber = parseFloat(firstNumber);
+    secondNumber = parseFloat(secondNumber);
     switch (operator) {
         case "+":
             return add(firstNumber, secondNumber);
@@ -47,3 +49,75 @@ let screen = document.querySelector('.screen');
 function render(text) {
     screen.innerText = text;
 }
+
+let numbers = document.querySelectorAll(".number");
+numbers.forEach(number => {
+    number.addEventListener('click', clickNumber);
+})
+
+let operators = document.querySelectorAll(".operator")
+operators.forEach(operator => {
+    operator.addEventListener('click', clickOperator);
+})
+
+let comma = document.querySelector(".comma");
+comma.addEventListener('click', clickComma);
+
+let fc = document.querySelectorAll(".fc");
+fc.forEach(element => {
+    element.addEventListener('click', clickFc);
+})
+
+
+function clickFc(e) {
+    let fc = e.target.innerText;
+    if (fc == "AC") {
+        firstNumber = "";
+        secondNumber = "";
+        operator = "";
+    } else if (fc == "C") {
+        secondNumber = secondNumber.slice(0, -1);
+    }
+    render(secondNumber);
+
+}
+
+function clickComma(e) {
+    if (secondNumber.includes(".")) {
+
+    } else {
+        secondNumber += ".";
+    }
+    render(secondNumber);
+}
+
+function clickNumber(e) {
+    console.log(e.target.innerText);
+    secondNumber += e.target.innerText.toString();
+    render(secondNumber);
+}
+
+function clickOperator(e) {
+
+    if (operator === '=') {
+        render(secondNumber);
+    }
+
+    if (firstNumber !== '' && secondNumber !== '' && operator !== '' && operator !== '=') {
+        let newNumber = operate(firstNumber, secondNumber, operator);
+        if (newNumber !== "") {
+            firstNumber = newNumber.toFixed(5);
+        }
+
+        secondNumber = "";
+        render(newNumber);
+    }
+
+
+    if (firstNumber === '') {
+        firstNumber = secondNumber
+        secondNumber = "";
+    }
+    operator = e.target.innerText.toString();
+}
+
